ee42609274e5985022bfb5ba35066255e0ab7d49
PauloDelphos <paulodelphos@gmail.com>
2022-03-05 22:11:06 -0300

    criado app

---
+++ todo/src/App.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #17181F;
+    color: #797A81;
+    min-height: 100vh;
+
+`;
+
---
+++ todo/src/App.tsx
@@ -0,0 +1,12 @@
+import * as C from './App.styles';
+
+const App = () => {
+  return (
+    <C.Container>
+      ...
+      ...
+    </C.Container>
+  );
+}
+
+export default App;
