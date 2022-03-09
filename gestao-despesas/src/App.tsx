307f44bedccd9789c6bf5330722766a2b548854d
PauloDelphos <paulodelphos@gmail.com>
2022-03-08 23:39:27 -0300

    inicio Projeto gestao de despesas em React

---
+++ gestao-despesas/src/App.tsx
@@ -0,0 +1,23 @@
+import * as C from './App.styles';
+
+const App = ()=>{
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+       
+        {/** Área de informações */}
+        
+        {/** Área de inserção */}
+       
+        {/** Tabela de itens*/}
+
+
+      </C.Body>
+    </C.Container>
+  );
+}
+
+export default App;
---
+++ gestao-despesas/src/data/items.ts
@@ -0,0 +1,9 @@
+import {Item} from '../types/item' 
+
+
+export const items: Item[] = [
+    {date: new Date(2022, 2, 09), category:'food', title:'McDonalds', value: 32.12},
+    {date: new Date(2022, 2, 09), category:'food', title:'Burger King', value: 25},
+    {date: new Date(2022, 2, 09), category:'rent', title:'Aluguel Apt', value: 550},
+    {date: new Date(2022, 2, 09), category:'salary', title:'Salário ACME', value: 32.12},
+];
